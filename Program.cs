01c1819b3ac24350c94da1db955c5c429172abc0
Braccone <brahimjaddi94@gmail.com>
2017-03-24 21:23:10 +0100

    Ho risolto tutti i problemi

--- Program.cs
+++ Program.cs
@@ -6,30 +6,41 @@ namespace UnicamFattoriale
     {
         static void Main(string[] args)
         {
-            //Stampo dei messaggi di cortesia per l'utente, così lo guido all'utilizzo del programma
-            Console.WriteLine("Questo programma calcola il fattoriale di un numero");
-            Console.Write("Digita un numero e premi invio: ");
+            try
+            {
+                while(true){
+                    //Stampo dei messaggi di cortesia per l'utente, così lo guido all'utilizzo del programma
+                    Console.WriteLine("Questo programma calcola il fattoriale di un numero");
+                    Console.Write("Digita un numero intero e premi invio: ");
 
-            //Console.ReadLine si mette in attesa che l'utente digiti qualcosa e prema invio.
-            //Quando ha premuto invio, il testo che ha digitato lo assegno ad una variabile di tipo string.
-            string testoDigitatoDallUtente = Console.ReadLine();
+                    //Console.ReadLine si mette in attesa che l'utente digiti qualcosa e prema invio.
+                    //Quando ha premuto invio, il testo che ha digitato lo assegno ad una variabile di tipo string.
+                    string testoDigitatoDallUtente = Console.ReadLine();
 
-            //Interpreto il testo come un numero intero
-            //Attenzione: questa istruzione fallirà se l'utente ha digitato un testo
-            //che non è interpretabile come un numero! (Es. ABCD)
-            int numero = int.Parse(testoDigitatoDallUtente);
+                    //Interpreto il testo come un numero intero
+                    //Attenzione: questa istruzione fallirà se l'utente ha digitato un testo
+                    //che non è interpretabile come un numero! (Es. ABCD)
+                    int numero = int.Parse(testoDigitatoDallUtente);
+                    Console.WriteLine($"{numero}");
+                    //Calcolo il fattoriale: qui ci sono degli errori!
+                    int risultato = 1;
+                    int n = numero;
+                    while (n > 0) {
+                        risultato *= n;
+                        n--;
+                    }
 
-            //Calcolo il fattoriale: qui ci sono degli errori!
-            int risultato = 0;
-            while (numero >= 0) {
-                risultato *= numero;
-                numero--;
+                    //Stampo il risultato
+                    Console.WriteLine($"Il fattoriale di {numero} e' {risultato}");
+                    //Attendo che l'utente prema un tasto prima di uscire, altrimenti 
+                }
             }
-
-            //Stampo il risultato
-            Console.WriteLine($"Il fattoriale di {numero} e' {risultato}");
-            //Attendo che l'utente prema un tasto prima di uscire, altrimenti 
-            Console.ReadKey();
+            catch (System.Exception)
+            {
+                Console.WriteLine("Spiacente, non posso calcolare il fattoriale per questo valore.");
+                Console.ReadKey();
+            }
+            
         }
     }
 }
